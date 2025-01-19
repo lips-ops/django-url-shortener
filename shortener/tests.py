c7d5c5e80b1b343c767111c59a029a35211a5df1
lipsa <lipsa.bhadaniya@primesophictech.com>
2025-01-19 17:43:02 +0530

    testcases

--- shortener/tests.py
+++ shortener/tests.py
@@ -20,11 +20,7 @@ class URLShortenerTests(TestCase):
     def test_url_with_password(self):
         response = self.client.post(reverse('shorten_url'), {'url': 'https://example.com', 'password': 'securepass'})
         short_url = response.json().get('short_url')
-
-        # Test accessing with correct password
         response = self.client.get(f'/r/{short_url.split("/")[-1]}/?password=securepass')
-        self.assertEqual(response.status_code, 302)  # Redirect to original URL
-
-        # Test accessing with incorrect password
+        self.assertEqual(response.status_code, 302)
         response = self.client.get(f'/r/{short_url.split("/")[-1]}/?password=wrongpass')
         self.assertEqual(response.status_code, 403)
